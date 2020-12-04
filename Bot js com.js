8d4cee1279154ce0d4f5949b9a18c65a9feb34aa
muhammadzaki693 <75458189+muhammadzaki693@users.noreply.github.com>
2020-12-04 08:31:26 +0700

    Create Bot js com.js

---
+++ Bot js com.js
@@ -0,0 +1,22 @@
+{
+  "endpoints": [
+//you can delete this you need to write + to space command in vacefron command
+    "GET: /carreverse?text=TEXT", 
+    "GET: /changemymind?text=TEXT", 
+    "GET: /distractedbf?boyfriend=USER_AVATAR&woman=USER_AVATAR&girlfriend=USER_AVATAR", 
+    "GET: /ejected?name=TEXT&impostor=BOOL&crewmate=black|blue|brown|cyan|darkgreen|lime|orange|pink|purple|red|white|yellow", 
+    "GET: /emergencymeeting?text=TEXT", 
+    "GET: /firsttime?user=USER_AVATAR", 
+    "GET: /grave?user=USER_AVATAR", 
+    "GET: /iamspeed?user=USER_AVATAR", 
+    "GET: /icanmilkyou?user1=USER_AVATAR&user2=USER_AVATAR", 
+    "GET: /heaven?user=USER_AVATAR", 
+    "GET: /npc?text1=TEXT&text2=TEXT", 
+    "GET: /rankcard?username=TEXT&avatar=URL&level=INT&rank=INT&currentxp=INT&nextlevelxp=INT&previouslevelxp=INT[&custombg=URL|HEX][&xpcolor=HEX][&isboosting=BOOL]", 
+    "GET: /stonks?user=USER_AVATAR[&notstonks=BOOL]", 
+    "GET: /tableflip?user=USER_AVATAR", 
+    "GET: /water?text=TEXT", 
+    "GET: /wide?image=IMAGE_URL"
+  ], 
+//original vommand in https://vacefron.nl/api
+}
