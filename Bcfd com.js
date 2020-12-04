d277756b168f80b89fe5fc757fbf700f8b127d13
muhammadzaki693 <75458189+muhammadzaki693@users.noreply.github.com>
2020-12-04 12:03:01 +0700

    Update Bcfd com.js

--- Bcfd com.js
+++ Bcfd com.js
@@ -1,22 +1,22 @@
 {
   "Bcfd api vacefron": [
 //you can delete this you need to write + to space command in vacefron command
-    "Copy: /carreverse?text=$messageAfterCommand", 
-    "Copy: /changemymind?text=$messageAfterCommand", 
-    "Copy: /distractedbf?boyfriend=$avatar&woman=USER_AVATAR&girlfriend=USER_AVATAR", 
-    "Copy: /ejected?name=TEXT&impostor=BOOL&crewmate=black|blue|brown|cyan|darkgreen|lime|orange|pink|purple|red|white|yellow", 
-    "Copy: /emergencymeeting?text=TEXT", 
-    "Copy: /firsttime?user=USER_AVATAR", 
-    "Copy: /grave?user=USER_AVATAR", 
-    "Copy: /iamspeed?user=USER_AVATAR", 
-    "Copy: /icanmilkyou?user1=&user2=USER_AVATAR", 
-    "Copy: /heaven?user=mentionedAvatar", 
-    "Copy: /npc?text1=$messageAfterCommand&text2=$messageAfterCommand", 
-    "Copy: /rankcard?username=$messageAfterCommand&avatar=$avatar&level=INT&rank=INT&currentxp=INT&nextlevelxp=INT&previouslevelxp=INT[&custombg=$avatar|HEX][&xpcolor=HEX][&isboosting=BOOL]", 
-    "Copy: /stonks?user=USER_AVATAR[&notstonks=BOOL]", 
-    "Copy: /tableflip?user=$mentionedAvatar", 
-    "Copy: /water?text=$messageAfterCommand", 
-    "Copy: /wide?image=$mentionedAvatar"
+    "Copy: vacefron.nl/api/carreverse?text=$messageAfterCommand", 
+    "Copy: vacefron.nl/api/changemymind?text=$messageAfterCommand", 
+    "Copy: vacefron.nl/api/distractedbf?boyfriend=$avatar&woman=$mentionedAvatar&girlfriend=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/ejected?name=$mentionedAvatar&impostor=BOOL&crewmate=$random{black|blue|brown|cyan|darkgreen|lime|orange|pink|purple|red|white|yellow}", 
+    "Copy: vacefron.nl/api/emergencymeeting?text=$messageAfterCommand", 
+    "Copy: vacefron.nl/api/firsttime?user=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/grave?user=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/iamspeed?user=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/icanmilkyou?user1=$avatar&user2=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/heaven?user=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/npc?text1=$messageAfterCommand&text2=$messageAfterCommand", 
+    "Copy: vacefron.nl/api/rankcard?username=$messageAfterCommand&avatar=$avatar&level=INT&rank=INT&currentxp=INT&nextlevelxp=INT&previouslevelxp=INT[&custombg=$avatar|HEX][&xpcolor=HEX][&isboosting=BOOL]", 
+    "Copy: vacefron.nl/api/stonks?user=$mentionedAvatar[&notstonks=BOOL]", 
+    "Copy: vacefron.nl/api/tableflip?user=$mentionedAvatar", 
+    "Copy: vacefron.nl/api/water?text=$messageAfterCommand", 
+    "Copy: vacefron.nl/api/wide?image=$mentionedAvatar"
   ], 
 //original vommand in https://vacefron.nl/api
 //you can edit $avatar>$mentionedAvatar or $mentionedAvatar>$avatar
