fcee2ae650c1e6010068c4c19d0c0b42295666e4
muhammadzaki693 <75458189+muhammadzaki693@users.noreply.github.com>
2020-12-04 08:48:20 +0700

    Create Bcfd com.js

---
+++ Bcfd com.js
@@ -0,0 +1,23 @@
+{
+  "Bcfd api vacefron": [
+//you can delete this you need to write + to space command in vacefron command
+    "Copy: /carreverse?text=$messageAfterCommand", 
+    "Copy: /changemymind?text=$messageAfterCommand", 
+    "Copy: /distractedbf?boyfriend=$avatar&woman=USER_AVATAR&girlfriend=USER_AVATAR", 
+    "Copy: /ejected?name=TEXT&impostor=BOOL&crewmate=black|blue|brown|cyan|darkgreen|lime|orange|pink|purple|red|white|yellow", 
+    "Copy: /emergencymeeting?text=TEXT", 
+    "Copy: /firsttime?user=USER_AVATAR", 
+    "Copy: /grave?user=USER_AVATAR", 
+    "Copy: /iamspeed?user=USER_AVATAR", 
+    "Copy: /icanmilkyou?user1=&user2=USER_AVATAR", 
+    "Copy: /heaven?user=mentionedAvatar", 
+    "Copy: /npc?text1=$messageAfterCommand&text2=$messageAfterCommand", 
+    "Copy: /rankcard?username=$messageAfterCommand&avatar=$avatar&level=INT&rank=INT&currentxp=INT&nextlevelxp=INT&previouslevelxp=INT[&custombg=$avatar|HEX][&xpcolor=HEX][&isboosting=BOOL]", 
+    "Copy: /stonks?user=USER_AVATAR[&notstonks=BOOL]", 
+    "Copy: /tableflip?user=$mentionedAvatar", 
+    "Copy: /water?text=$messageAfterCommand", 
+    "Copy: /wide?image=$mentionedAvatar"
+  ], 
+//original vommand in https://vacefron.nl/api
+//you can edit $avatar>$mentionedAvatar or $mentionedAvatar>$avatar
+}
